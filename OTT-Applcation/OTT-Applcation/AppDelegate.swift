6d522e1169c23f59c17c893d992b9bba44b52809
Rahul-iOS-ttn <rahul.sharma1@tothenew.com>
2021-06-28 19:20:22 +0530

    Added external POD support and test fix

--- OTT-Applcation/OTT-Applcation/AppDelegate.swift
+++ OTT-Applcation/OTT-Applcation/AppDelegate.swift
@@ -6,32 +6,77 @@
 //
 
 import UIKit
+import SocialLogin
 
-@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        let navController = UINavigationController(rootViewController: ViewController.viewController())
+//        window?.rootViewController = navController
+        
+        
+        do {
+            try ANAuthService.shared.setUpLoginService(setUpConfig: ANAuthServiceSetupConfig(application: application, loginOptions: launchOptions, googleClientId: "1086721698817-gg3c8o60hht6gm7b3qljc6s01ea4qtoe.apps.googleusercontent.com", keychainServiceName: "com.loginmodule"))
+            
+        } catch ANSignInError.setupError(let typeOfSignIn) {
+            
+            switch typeOfSignIn {
+            case .google:
+                print("Google login set up failed")
+            case .facebook:
+                print("Facebook login set up failed")
+            case .apple:
+                print("Apple sing in set up failed")
+            default:
+                print("Other login set up failed")
+            }
+        }  catch {
+            print("Unkowen error")
+        }
+        
+        
+        
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+       
+        return ANAuthService.shared.handle(app, open: url, options: options)
+        
+
+    }
+    
+    
 }
+
 
--- OTT-Applcation/OTT-Applcation/ViewController.swift
+++ OTT-Applcation/OTT-Applcation/ViewController.swift
@@ -7,15 +7,181 @@
 
 import UIKit
 import SocialLogin
+import AuthenticationServices
 
+@available(iOS 13.0, *)
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginWithFbButton: UIButton!
+    @IBOutlet weak var loginWithGoogleButton: UIButton!
+    @IBOutlet weak var loginWithAppleButton: ASAuthorizationAppleIDButton!
+    
+    @IBOutlet weak var signOut: UIButton!
+    @IBOutlet weak var tokenLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var loggedInView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Validator.sayHello()
         print("This email is valid: ", Validator.validEmail("rahul@rahul.com"))
+        prepareViewOnLoad()
     }
+    
+    var observerToken: NSObjectProtocol?
+}
 
+// MARK: - View Setup
+
+@available(iOS 13.0, *)
+extension ViewController {
+    func prepareViewOnLoad () {
+        view.backgroundColor = .white
+        
+        
+        observeCredential()
+        loginWithFbButton.setTitle("Connect with Facebook", for: .normal)
+        
+        
+        loginWithGoogleButton.setTitle("Connect with Google", for: .normal)
+        
+        loginWithAppleButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        loginWithFbButton.addTarget(self, action: #selector(didTapFacebook), for: .touchUpInside)
+        loginWithGoogleButton.addTarget(self, action: #selector(didTapGoogle), for: .touchUpInside)
+        
+        signOut.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        
+        
+        ANAuthService.shared.hasPreviousSignIn { [weak self] signedIn in
+            if signedIn {
+                
+                ANAuthService.shared.restoreUserSession {[weak self] (result) in
+                    
+                    switch result {
+                    case .success(let successObj):
+                        print("login via sucessfull \(String(describing: successObj.authToken))")
+                        self?.updateView(auth: successObj)
+                        
+                    case .failure(let error):
+                        print("Login Failed: \(error.localizedDescription)")
+                    }
+                }
+                
+            }else {
+                
+                ANAuthService.shared.restoreUserSessionExplicit(with: .apple) {[weak self] (result) in
+                    
+                    switch result {
+                    case .success(let successObj):
+                        print("login via sucessfull \(String(describing: successObj.authToken))")
+                        self?.updateView(auth: successObj)
+                        
+                    case .failure(let error):
+                        print("Login Failed: \(error.localizedDescription)")
+                    }
+                }
+                
+                self?.loggedInView.isHidden = true
+            }
+        }
+    }
+}
+
+
+// MARK: Actions
+@available(iOS 13.0, *)
+extension ViewController: CredentialRevokedObservable {
+    
+    
+    func credentialRevoked() {
+        
+        ANAuthService.shared.signOut() { [self] result in
+            switch result {
+            case .success:
+                self.cleanView()
+            case .failure(let error):
+                print("Could not logout with error - \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
+    @objc func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapFacebook() {
+        
+        ANAuthService.shared.signIn(with: .facebook, fromView: self) {[weak self] (result) in
+            
+            switch result {
+            case .success(let successObj):
+                print("login via Facebook sucessfull \(String(describing: successObj.authToken))")
+                self?.updateView(auth: successObj)
+                
+            case .failure(let error):
+                print("Login Failed: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @objc func didTapGoogle() {
+        
+        ANAuthService.shared.signIn(with: .google, fromView: self) {[weak self] (result) in
+            
+            switch result {
+            case .success(let successObj):
+                print("login via google sucessfull \(String(describing: successObj.authToken))")
+                self?.updateView(auth: successObj)
+                
+            case .failure(let error):
+                print("Login Failed: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// - Tag: perform_applied_request
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        
+        ANAuthService.shared.signIn(with: .apple, fromView: self) {[weak self] (result) in
+            
+            switch result {
+            case .success(let successObj):
+                print("login via apple sucessfull \(String(describing: successObj.authToken))")
+                self?.updateView(auth: successObj)
+                
+            case .failure(let error):
+                print("Login Failed: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @objc func didTapSignOut() {
+        
+        ANAuthService.shared.signOut() { [self] _ in
+            self.cleanView()
+        }
+        
+    }
+    
+    
+    func updateView(auth : ANUserAuth) {
+        loggedInView.isHidden = false
+        nameLabel.text = auth.anUser?.profileName
+        tokenLabel.text = auth.authToken
+        idLabel.text = auth.anUser?.userId
+    }
+    
+    func cleanView() {
+        loggedInView.isHidden = true
+        nameLabel.text = nil
+        tokenLabel.text = nil
+        idLabel.text = nil
+    }
+    
 }
 
